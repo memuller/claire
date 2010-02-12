fb48ac8d9df367b01046bfcbe8253f89cd44b347
Matheus E. Muller <hello@memuller.com>
2010-02-12 12:08:27 -0200

    Implemented state machine using state-fu; due to problems with AASM.

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,50 +1,66 @@
 class Video
   include MongoMapper::Document
   include StateFu
+  include Paperclip
   
-  #mongo keys  
-  key :status, Symbol
+  def logger
+    Rails.logger
+  end
+                     
+  #state machine controller  
+  key :state_fu_field, String
+  #general info
   key :name, String
+  key :subtitle, String
   key :description, String
   key :tags, String
   key :category_id, ObjectId
   key :subcategory_id, ObjectId
   key :desired_formats, Array
+  #metadata
+  key :duration, Integer
+  #statistics
   key :num_ratings, Integer
   key :value_ratings, Integer
   key :rating, Integer
-  key :num_views, Integer  
+  key :num_views, Integer
   
+  #raw/thumbnails info
+  key :video_file_name, String
+  key :video_file_size, Integer
+  key :video_content_type, String
+  key :video_updated_at, DateTime
+  
+  has_attached_file :video, :styles => { :thumb_medium => "215x120", :thumb_large => "500x281" }, 
+                    :processors => [:video_thumbnail],
+                    :path => "#{RAILS_ROOT}/public/videos/:id/:style.:video_extensions"
   #relationships
   belongs_to :category
   belongs_to :subcategory
-  
-  machine :status do 
+
+  machine :default do 
     #starting up, video's not here yet
     state :initialized do 
       requires :name
-      event :next!, :to => :raw_received
+      event :check_upload, :to => :raw_received
     end
+    
     #raw's here
     state :raw_received do
       requires :raw_file_okay?
-      event :next!, :to => :thumbnailing
+      event :convert, :to => :converting
     end
-    #let's thumbnail it
-    state :thumbnailing do
-      requires :raw_format_okay?
-      on_entry :work_on_thumbnails
-      event :next!, :to => :converting
-    end
+    
     #them convert it
     state :converting do
+      requires :raw_format_okay?
       on_entry :work_on_conversions
-      event :next!, :to => :publishing
+      event :publish, :to => :publishing
     end
     #publish the converted files
     state :publishing do
       on_entry :work_on_publishing
-      event :next!, :to => :archiving
+      event :archive, :to => :archiving
     end
     #archive the raw
     state :archiving do
@@ -55,19 +71,18 @@ class Video
     state :error
     state :done 
     #...and their transitions
-    event :error!, :from => :ALL, :to => :error
-    event :nothing_to_do!, :from => :ALL, :to => :done 
-       
-  end
+    event :error, :from => :ALL, :to => :error
+    event :done, :from => :ALL, :to => :done 
   
-  #save states to the database after each transition
-  states do
-    accepted { save! }
-    
-  #returns the path for this video's raw file
-  def raw_file_path                          
-    "#{RAILS_ROOT}/videos/raw/#{self._id}"    
-  end
+    #save states to the database after each transition
+    states do
+      accepted { 
+        puts "changed state"
+        #status = current_state
+        save! 
+      }
+    end  
+  end                              
   
   #writes an uploaded raw to it's destination
   def write_uploaded_file(upload)
@@ -76,28 +91,32 @@ class Video
     end
   end
   
+  #returns raw file path
+  def uploaded_file_path
+    "#{RAILS_ROOT}/public/videos/#{id}/original.#{video_file_name.split(".").last}"
+  end
+  
   #checks if raw exists
   def raw_file_okay?
-    File.exists uploaded_file_path
+    File.exists? uploaded_file_path
   end
   
   #checks if raw is a valid, convertible video
-  def format_okay?
-    
+  def raw_format_okay?
+     begin
+       inspector = RVideo::Inspector.new :file => uploaded_file_path
+       duration = inspector.duration
+       save! and return true
+     rescue ArgumenError => e
+     	 return false
+     end     
   end
   
   #== WORKERS
-  def work_on_thumbnails
-    
-  end
-  
-  def make_thumbnails
-    
-  end
   
   
   
-  def publish(target)
+  def work_on_publishing(target)
     raise NotImplementedError unless [:youtube, :localhost].include? target
   end
   
