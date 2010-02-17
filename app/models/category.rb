9dbe22c53f3c654ae061f353b55b9ab2ac09dd7b
Matheus E. Muller <hello@memuller.com>
2010-02-17 12:32:30 -0200

    Added an imagem attachment to the categories; representing their image header.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,19 @@
 class Category
   include MongoMapper::Document
-  key :name, String, :required => true
+	include Paperclip
+  
+	key :name, String, :required => true
   key :description, String
   
   many :subcategories
+
+	#raw/thumbnails info
+  key :image_file_name, String
+  key :image_file_size, Integer
+  key :image_content_type, String
+  key :image_updated_at, DateTime
+  
+  has_attached_file :image, :styles => { :thumb => "215x120"}, 
+                    :path => "#{RAILS_ROOT}/public/categories/:id/:style.jpg"
+
 end
