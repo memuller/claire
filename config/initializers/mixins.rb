9124a5d71da6e158cf04b455aff0f1df277fa374
Matheus E. Muller <hello@memuller.com>
2010-03-04 18:56:57 -0300

    Added a few usefull mixins.

---
+++ config/initializers/mixins.rb
@@ -0,0 +1,32 @@
+class Symbol
+	def to_class
+		self.to_s.classify.constantize rescue return nil
+	end
+end
+
+module Kernel
+	def retryable(args = {}, &block)
+	  options = { :tries => 1, :on => Exception }.merge args
+	  retry_exception, retries = args[:on], args[:tries]
+
+	  begin
+	    return yield
+	  rescue 
+	    retry if (retries -= 1) > 0
+	  end
+	  yield
+	end	
+end
+
+class WorkerLogger
+	def ok args
+		puts "OK: " + args.to_s + "."
+	end
+	def error args
+		puts "ERROR: " + args.to_s + ""
+	end
+	def start args
+		puts "START: " + args.to_s + "..."
+	end
+end
+LOGGER = WorkerLogger.new
