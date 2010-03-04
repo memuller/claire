36ec7d7cc39ad4d0966f21c4ed5d7da6f2f84cee
Matheus E. Muller <hello@memuller.com>
2010-03-04 16:03:36 -0300

    Publisher now working perfectly for youtube, as described by [#28]

--- app/workers/publisher_worker.rb
+++ app/workers/publisher_worker.rb
@@ -3,14 +3,35 @@ class PublisherWorker < Workling::Base
     CONFIG["publishers"]
   end
   def publish params
-    errors = [] and video = Video.find(params[:video_id])
-    uploader = YouTubeG::Upload::VideoUpload.new(config['youtube']['username'], config['youtube']['password'], config['youtube']['api_key'])
-    uploader.upload File.open("video."), :title => 'test', :description => '', :category => 'People', :keywords => %w[bla]
+		video = Video.find params[:video_id]
+		errors = []
+ 		# loops on all publishers specified on the config file
+		config.each do |item|
+			LOGGER.start "Publishing video #{video.id} on #{item.first.classify}"
+			# gets a publisher from the Publishers module with the specified name
+			klass = Publishers.const_get(item.first.classify)
+			
+			# builds an options array to initialize the publisher.
+			# just converts the string keys from the yaml to symbols.
+			options = {}
+			item.last.each do |k,v|
+				options.merge!({k.to_sym => v})
+			end
+			options.merge!({:video => video})
+			
+			# instantiate the publisher with those options
+			publisher = klass.new(options)
+			
+			# publishes; if it doesn't return true, get its errors.
+			unless (result = publisher.publish!) == true
+				errors << result
+			end
+		end
     
     if errors.empty?
       video.archive!
     else
-      video.errors << errors
+      video.worker_errors = video.worker_errors | errors
       video.save! and video.error!
     end
        
---
+++ lib/publishers/youtube.rb
@@ -0,0 +1,129 @@
+module Publishers
+	class Youtube
+		
+		# requirements
+		require 'net/http' 
+		require 'net/https' 
+		require 'rexml/document' 
+		
+		# URL-building constants
+		AUTH_HOST = "www.google.com"
+		UPLOAD_HOST = 'uploads.gdata.youtube.com'		
+		AUTH_PATH = '/youtube/accounts/ClientLogin'
+		UPLOAD_PATH = "/feeds/api/users/username/uploads"
+		
+		# objects accessor methods
+		attr_accessor :username, :password, :api_key, :video, :category
+		attr_reader :token
+		
+		def initialize args={}
+			unless args[:video].is_a? Video and args[:username] and args[:password] and args[:api_key]
+				raise ArgumentError, "Requires username, password, api_key and a valid video."
+			end
+			raise ArgumentError, "Username should be an google email, not an youtube user" unless args[:username].include? "@"
+			args.each{ |k,v| instance_variable_set "@#{k}", v }			
+			#auths
+			get_auth_token			
+		end
+		
+		# actually POSTs the upload request. 
+		# - on success: returns true and assigns an youtube video id
+		# - on failure: returns an array of errors
+		def publish!
+			read_video
+			@boundary_string = "f93dcbA3" 
+			request_body = build_upload_request
+			http = Net::HTTP.new UPLOAD_HOST
+			
+		  request_headers = {
+				'X-GData-Key' => "key=#{@api_key}",
+		  	'Slug' => @video_file_name,
+		  	'Content-Type' => %(multipart/related; boundary="#{@boundary_string}"),
+		  	'Content-Length' => request_body.length.to_s,
+		  	'Connection' => 'close',
+		  	'Authorization' => "GoogleLogin auth=#{@token}"
+		  }
+		
+      response, body = retryable :tries => 3 do
+      	http.post( UPLOAD_PATH.sub('username', @youtube_user.to_s), request_body, request_headers )
+      end		  
+      
+			if response.code == '201'
+				xml = REXML::Document.new body
+				video_id = xml.elements['entry'][0].text.split('/').last
+      	@video.update_attributes :youtube_id => video_id
+				@video.save!
+				LOGGER.ok "Published video #{@video.id} to Youtube as #{video_id}."
+				return true
+			else
+				LOGGER.error "Failed to publish video #{@video.id} to Youtube with error #{response.code} - #{response.body}"				
+				return "Youtube upload failed with status code #{response.code}. \n Response body: \n #{response.body}" 
+      end
+		end
+		
+		# reads the video raw file; returns its filename and it's content as string.
+		def read_video
+			@video_file_content = File.open(@video.uploaded_file_path, 'r'){ |io| io.read }
+			@video_file_name = @video.uploaded_file_path.split("/").last			
+		end
+		
+		# returns an upload request to be POST'ed
+		def build_upload_request
+request = <<"DATA"
+--#{@boundary_string}		
+Content-Type: application/atom+xml; charset=UTF-8
+
+<?xml version="1.0"?>
+<entry xmlns="http://www.w3.org/2005/Atom"
+  xmlns:media="http://search.yahoo.com/mrss/"
+  xmlns:yt="http://gdata.youtube.com/schemas/2007">
+  <media:group>
+    <media:title type="plain"> #{@video.title} </media:title>
+    <media:description type="plain"> #{@video.description} </media:description>
+    <media:category scheme="http://gdata.youtube.com/schemas/2007/categories.cat">People
+    </media:category>
+    <media:keywords> #{@video.tags.join(", ")} </media:keywords>
+  </media:group>
+</entry>
+--#{@boundary_string}
+Content-Type: #{@video.video_content_type}
+Content-Transfer-Encoding: binary
+
+#{@video_file_content}
+--#{@boundary_string}--
+DATA
+			request
+		end
+		
+		# returns an GData ClientAuth token for the specified user.
+		# also sets @youtube_user with the youtube user for the given G.Account.
+		def get_auth_token 
+			# new http request; sends login info as url params and a simple header
+			http = Net::HTTP.new(AUTH_HOST, 443); http.use_ssl = true
+			data = "Email=#{@username}&Passwd=#{@password}&service=youtube&source=Claire"
+			headers = {'Content-Type' => 'application/x-www-form-urlencoded'}
+			# a dumb retry block
+			tries = 0
+			begin
+				response, data = http.post AUTH_PATH, data, headers
+			rescue
+				tries += 1
+				if tries < 3
+					retry
+				else
+					raise "Exceded maximum number of retries for auth request. Possibly, the server is quite busy."
+				end
+			end  
+      
+			# if response code was OK, return the auth token
+			if response.code == '200'
+				@token = data.scan /Auth=(.*)/
+			  @youtube_user = data.scan /YouTubeUser=(.*)/
+			  return @token
+			else
+				raise ArgumentError, "Invalid login credentials provided; GData tells you that: #{data}"
+			end
+		end
+			
+	end
+end
