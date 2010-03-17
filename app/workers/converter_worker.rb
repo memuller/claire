9941affbe8146805980d76b1acceba8d21ceb7ce
Matheus E. Muller <hello@memuller.com>
2010-03-17 10:29:32 -0300

    Fixing recipe issues with x264

--- app/workers/converter_worker.rb
+++ app/workers/converter_worker.rb
@@ -15,7 +15,6 @@ class ConverterWorker < Workling::Base
 		args['options'].each do |k,v|
     	recipe += " -#{k} #{v}"
     end
-    puts recipe
     if args['format'] == "flv"
       recipe += " - | flvtool2 -U stdin #{args['output']}"
     else
